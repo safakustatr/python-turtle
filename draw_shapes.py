7e30ce967cd75dca919099bf13d91d0b11219fc1
Safak <safakustatr@gmail.com>
2025-06-23 17:03:53 +0300

    Update draw_shapes.py

--- draw_shapes.py
+++ draw_shapes.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
 import random
 
 timmy = Turtle()
@@ -15,26 +15,10 @@ def draw_shape(edge):
         timmy.forward(100)
         timmy.right(degree)
 
-# for i in range(3,11):
-#     timmy.pencolor(generate_random_color())
-#     draw_shape(i)
-
-def move_random():
+for i in range(3,11):
     timmy.pencolor(generate_random_color())
-    left_or_right = random.randint(0, 3)
-    if left_or_right == 0:
-        timmy.right(90)
-    elif left_or_right == 1:
-        timmy.left(90)
-    elif left_or_right == 2:
-        timmy.right(180)
-    else:
-        pass
-    timmy.forward(50)
+    draw_shape(i)
 
-timmy.pensize(10)
-for _ in range(100):
-    move_random()
 
 
 
