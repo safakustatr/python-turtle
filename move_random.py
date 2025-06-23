d5743c7897a50da27206c7826a2f088d5b4755af
Safak <safakustatr@gmail.com>
2025-06-23 17:02:17 +0300

    Update move_random.py

--- move_random.py
+++ move_random.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
 import random
 
 timmy = Turtle()
@@ -8,16 +8,6 @@ def generate_random_color():
     g = random.randint(0, 100)/100
     b = random.randint(0, 100)/100
     return r, g, b
-
-def draw_shape(edge):
-    degree = 360 / edge
-    for _ in range(edge):
-        timmy.forward(100)
-        timmy.right(degree)
-
-# for i in range(3,11):
-#     timmy.pencolor(generate_random_color())
-#     draw_shape(i)
 
 def move_random():
     timmy.pencolor(generate_random_color())
